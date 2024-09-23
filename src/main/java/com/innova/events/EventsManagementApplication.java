cdb5d3b9f9914afd3155f0dfbc61797f66efa5a2
Juan Sebastian Charry Rodriguez <juan.charry@colombiacompra.gov.co>
2024-09-22 22:36:24 -0500

    add project

---
+++ src/main/java/com/innova/events/EventsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.innova.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innova/events/EventsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innova.events;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
